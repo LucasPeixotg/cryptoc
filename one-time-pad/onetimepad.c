01d27642881e8d55965cc5c91cae1e005b9f1b55
LucasPeixotg <Lucaspeixotopro@gmail.com>
2024-08-20 18:08:37 -0300

    reading file added

--- one-time-pad/onetimepad.c
+++ one-time-pad/onetimepad.c
@@ -1,14 +1,29 @@
 #include "onetimepad.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 // load a key from a file
 // returns:
 //      0 - successful
 //      1 - failed
-int otp_load_key_from_file(struct otp_system*, char* filepath);
+int otp_load_key(struct otp_system* crypto, char* filepath) {
+    FILE *file_ptr = fopen(filepath, "r");
+    if(file_ptr == NULL) {
+        // could not read file
+        fprintf(stderr, "\nError while opening the file\n");
+        return 1;
+    }
+
+    crypto->key = file_ptr;
+    return 0;
+}
 
 // decode byte using key,
 // updates the key while doing so
-byte otp_decode_byte(struct otp_system*, byte encrypted);
+byte otp_decode_byte(struct otp_system*, byte encrypted) {
+    // TODO:
+}
 
 // encode byte using key
 // updates the key while doing so
--- one-time-pad/onetimepad.h
+++ one-time-pad/onetimepad.h
@@ -4,16 +4,14 @@
 typedef unsigned char byte;
 
 struct otp_system {
-    byte* key;
-    unsigned int key_size;
-    unsigned int current_key;
+    FILE* key;
 };
 
 // load a key from a file
 // returns:
 //      0 - successful
 //      1 - failed
-int otp_load_key_from_file(struct otp_system*, char* filepath);
+int otp_load_key(struct otp_system*, char* filepath);
 
 // decode byte using key,
 // updates the key while doing so
